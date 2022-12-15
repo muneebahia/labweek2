60e17160e2ccee0517adcb2a3a98a29ca3bd0d0d
muneeba hia <muneeba.hia.pro@gmail.com>
2022-12-15 07:06:58 -0800

    sending files

---
+++ logo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main() {
+cout<<"                         -----                "<<endl;       
+cout<<"                        |#####|               "<<endl;
+cout<<"                   - ############# -          "<<endl;
+cout<<"                  - #             # -         "<<endl;
+cout<<"                 - #      CS       # -        "<<endl;
+cout<<"                - #                 # -       "<<endl;
+cout<<"               - #    DEPARTMENT     # -      "<<endl;
+cout<<"              - #                     # -     "<<endl;
+cout<<"               - #      OF UET       # -      "<<endl;
+cout<<"                - #                 # -       "<<endl;
+cout<<"                 - #   (LAHORE)    # -        "<<endl;
+cout<<"                  - #             # -         "<<endl;
+cout<<"                   - ############# -          "<<endl;
+cout<<"                        |#####|               "<<endl;
+cout<<"                         -----                "<<endl;
+
+
+}
---
+++ project.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main() {
+cout<<"#####################################################"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#          MY SECOND LAB OF PROGRAMMING             #"<<endl;
+cout<<"#                FUNDAMENTALS                       #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#                                                   #"<<endl;
+cout<<"#####################################################"<<endl;
+
+}
