e3ad57eb2263da2d061a7216de7caf2ebe39aeb2
muneeba hia <muneeba.hia.pro@gmail.com>
2022-12-18 10:17:09 -0800

    new tasks

---
+++ car.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main(){
+cout<<"                  _____________                      "<<endl;
+cout<<"                 /    |    |   \\                    "<<endl;
+cout<<"                /     |    |    \\                   "<<endl;
+cout<<"           ____/______|____|_____\\______            "<<endl;
+cout<<"          /                              \\          "<<endl;
+cout<<"         /      ...            ...        \\         "<<endl;
+cout<<"        /______.   .__________.   .________\\        "<<endl;
+cout<<"    __________  ...    ________...__________________ "<<endl;
+
+  
+}
---
+++ donkey.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main (){
+cout<<" ------------------------------    "<<endl;
+cout<<"        o    ^___^                 "<<endl;
+cout<<"          o  (oo )\\________       "<<endl;
+cout<<"             (__)\\         )\\/\\ "<<endl;
+cout<<"                 ||------w  |      "<<endl;
+cout<<"                 ||        ||      "<<endl;
+}
